7d959773e49736de48e37c487f680c86876787ca
structix <janek@schoff.it>
2023-10-31 11:44:05 +0100

    Refactor heuristics

---
+++ src/GraphXings/Gruppe4/Heuristics.java
@@ -0,0 +1,92 @@
+package GraphXings.Gruppe4;
+
+import GraphXings.Data.Coordinate;
+import GraphXings.Data.Graph;
+import GraphXings.Data.Vertex;
+import GraphXings.Game.GameMove;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+
+public class Heuristics {
+
+
+    /**
+     * Tries to minimize the amount of crossings in a graph g by testing for the min. amount of crossings.
+     * In the first move we can't find any crossings because we don't have enough coordinates.
+     * Therefore, we place the vertex in a corner of the canvas with the max. distance to the already placed vertex.
+     *
+     * @param g The graph object.
+     * @param usedCoordinates This contains an array of the canvas with already used coordinates.
+     * @param vertexCoordinates This is a map which outputs the coordinates for a given vertex.
+     * @param placedVertices The already placed vertices.
+     * @param width Width of the canvas.
+     * @param height Height of the canvas.
+     * @return A game move of the final decision.
+     */
+    public static Optional<GameMove> minimizeHeuristic(Graph g, int[][] usedCoordinates, HashMap<Vertex, Coordinate> vertexCoordinates, List<GameMove> gameMoves, HashSet<Vertex> placedVertices, int width, int height) {
+        if (placedVertices.size() < 3) {
+            // In this case we have to place more or less random coordinates
+            // because it's not possible to calculate crossings at this point.
+
+            GameMove lastGameMove = gameMoves.get(gameMoves.size() - 1);
+
+            var lastVertex = lastGameMove.getVertex();
+            var lastCoordinate = lastGameMove.getCoordinate();
+
+            // Get a neighbour vertex by iterating through the edges
+            Vertex neighbourVertex = null;
+            for (var e : g.getEdges()) {
+                if (e.getS().equals(lastVertex)) {
+                    neighbourVertex = e.getT();
+                    break;
+                }
+            }
+
+            // Place the vertex in a corner of the canvas with
+            // maximum distance to the placed vertex.
+            int maxDistance = 0;
+            Coordinate maxCoordinate = new Coordinate(0, 0);
+            for (int i = 0; i < width; i += width - 1) {
+                for (int k = 0; k < height; k += height - 1) {
+                    var tempCoord = new Coordinate(i, k);
+                    var distance = euclideanDistance(lastCoordinate, tempCoord);
+                    if (distance > maxDistance) {
+                        maxDistance = distance;
+                        maxCoordinate = tempCoord;
+                    }
+                }
+            }
+
+            return Optional.of(new GameMove(neighbourVertex, maxCoordinate));
+        }
+        return Optional.empty();
+    }
+
+
+    public static Optional<GameMove> maximizeHeuristic(Graph g, int[][] usedCoordinates, HashMap<Vertex, Coordinate> vertexCoordinates, List<GameMove> gameMoves, HashSet<Vertex> placedVertices, int width, int height) {
+        if (placedVertices.size() == 0) {
+            // Maximize is the first move of the game.
+            // Therefore, we place the vertex in the middle of the canvas
+            var firstVertex = g.getVertices().iterator().next();
+            var middleCoordinate = new Coordinate(width / 2, height / 2);
+            return Optional.of(new GameMove(firstVertex, middleCoordinate));
+        }
+        return Optional.empty();
+    }
+
+
+
+
+    /**
+     * Calculate the euclidean distance between two coordinates as integer value.
+     * @param p A coordinate object.
+     * @param q A coordinate object.
+     * @return The distance as integer.
+     */
+    public static int euclideanDistance(Coordinate p, Coordinate q) {
+        return (int)Math.sqrt(Math.pow(q.getX() - p.getX(), 2) + Math.pow(q.getY() - p.getY(), 2));
+    }
+}
--- src/GraphXings/Gruppe4/RTreePlayer.java
+++ src/GraphXings/Gruppe4/RTreePlayer.java
@@ -58,120 +58,6 @@ public class RTreePlayer implements Player {
         }
     }
 
-
-    /**
-     * Tries to maximize the amount of crossings in a graph g by moving the vertices near each other.
-     * This is currently experimental and therefore we use maximizeMove2.
-     *
-     * @param g The graph object.
-     * @param usedCoordinates This contains an array of the canvas with already used coordinates.
-     * @param vertexCoordinates This is a map which outputs the coordinates for a given vertex.
-     * @param placedVertices The already placed vertices.
-     * @param width Width of the canvas.
-     * @param height Height of the canvas.
-     * @return A game move of the final decision.
-     */
-    private GameMove maximizeMove(Graph g, int[][] usedCoordinates, HashMap<Vertex, Coordinate> vertexCoordinates, HashSet<Vertex> placedVertices, int width, int height) {
-        // This list contains the possible coordinates for the next move.
-        // We have to check if these coordinates are actually unused,
-        // therefore we need multiple candidates.
-        var coordinateCandidate = new HashMap<Vertex, Coordinate>();
-
-        // Calculate the middle point of the edge which is connected to its two vertices.
-        for (Vertex v : g.getVertices()) {
-            if (!placedVertices.contains(v)) {
-                var midpoint = getMidpoint(g, vertexCoordinates, v);
-                if (midpoint.isEmpty()) {
-                    // Invalid vertex, choose another one
-                    continue;
-                }
-
-                // Move the candidate position closer to the target position
-                //var candidatePosition = moveVertexCloser(vertexCoordinates.get(v), midpoint.get());
-                coordinateCandidate.put(v, midpoint.get());
-            }
-        }
-
-        // Check if one of the coordinate candidates are possible game moves
-        for (var vertex : coordinateCandidate.keySet()) {
-            if (isCoordinateFree(usedCoordinates, coordinateCandidate.get(vertex))) {
-                return new GameMove(vertex, coordinateCandidate.get(vertex));
-            }
-        }
-
-
-        // If we haven't found anything yet use fallback strategy
-        // -> use the first free coordinate
-        var coordinate = new Coordinate(0, 0);
-        for (int i = 0; i < width; i++) {
-            for (int k = 0; k < height; k++) {
-                // Find a free coordinate
-                if (usedCoordinates[i][k] == 0) {
-                    coordinate = new Coordinate(i, k);
-                    break;
-                }
-            }
-        }
-
-        // Find an unplaced vertex
-        Vertex vertex = null;
-        for (var v : g.getVertices()) {
-            if (!placedVertices.contains(v)) {
-                vertex = v;
-            }
-        }
-        // This case should never happen
-        return new GameMove(vertex, coordinate);
-    }
-
-    /**
-     * Calculate the middle point for a selected vertex and its neighbours.
-     * @param g The graph object
-     * @param vertexCoordinates This is a map which outputs the coordinates for a given vertex.
-     * @param selectedVertex The selected vertex
-     * @return Middle point coordinates or none
-     */
-    private Optional<Coordinate> getMidpoint(Graph g, HashMap<Vertex, Coordinate> vertexCoordinates, Vertex selectedVertex) {
-        HashMap<Vertex, Coordinate> neighbours = new HashMap<>();
-        // Get the adjacent edges of the selected vertex
-        // and calculate the middle point of the edges.
-        for (Edge e : g.getIncidentEdges(selectedVertex)) {
-            Vertex source = e.getS();
-            Vertex target = e.getT();
-
-            // Check if source vertex is a neighbour of selected vertex and check if this neighbour has coordinates
-            if (!source.equals(selectedVertex) && vertexCoordinates.get(source) != null) {
-                neighbours.put(source, vertexCoordinates.get(source));
-            }
-            // Do the same for the target vertex
-            if (!target.equals(selectedVertex) && vertexCoordinates.get(target) != null) {
-                neighbours.put(target, vertexCoordinates.get(target));
-            }
-        }
-
-        // At this point we should have either two neighbours or we can't use the selected vertex
-        if (neighbours.values().size() != 2) {
-            // Invalid selected vertex
-            return Optional.empty();
-        }
-
-        // Calculate the middle point coordinate
-        int x = neighbours.values().stream().mapToInt(Coordinate::getX).sum();
-        int y = neighbours.values().stream().mapToInt(Coordinate::getY).sum();
-
-        // Return the half of each sum to get the middle of x/y
-        return Optional.of(new Coordinate(x / 2, y / 2));
-    }
-
-
-    // Currently not in use. This should be used in the middle point maximizing strategy
-    private Coordinate moveVertexCloser(Coordinate vertexPosition, Coordinate targetPosition) {
-        // Move the vertex closer to the target position
-        int x = vertexPosition.getX() + targetPosition.getX();
-        int y = vertexPosition.getY() + targetPosition.getY();
-        return new Coordinate(x / 2, y / 2);
-    }
-
     /**
      * Helper function to check whether a coordinate is free or in use.
      * @param usedCoordinates The usedCoordinates array.
@@ -197,40 +83,9 @@ public class RTreePlayer implements Player {
      * @return A game move of the final decision.
      */
     private GameMove minimizeMove(Graph g, int[][] usedCoordinates, HashMap<Vertex, Coordinate> vertexCoordinates, List<GameMove> gameMoves, HashSet<Vertex> placedVertices, int width, int height) {
-        if (placedVertices.size() < 3) {
-            // In this case we have to place more or less random coordinates
-            // because it's not possible to calculate crossings at this point.
-
-            GameMove lastGameMove = gameMoves.get(gameMoves.size()-1);
-
-            var lastVertex = lastGameMove.getVertex();
-            var lastCoordinate = lastGameMove.getCoordinate();
-
-            // Get a neighbour vertex by iterating through the edges
-            Vertex neighbourVertex = null;
-            for (var e : g.getEdges()) {
-                if (e.getS().equals(lastVertex)) {
-                    neighbourVertex = e.getT();
-                    break;
-                }
-            }
-
-            // Place the vertex in a corner of the canvas with
-            // maximum distance to the placed vertex.
-            int maxDistance = 0;
-            Coordinate maxCoordinate = new Coordinate(0, 0);
-            for (int i = 0; i < width; i += width - 1) {
-                for (int k = 0; k < height; k += height - 1) {
-                    var tempCoord = new Coordinate(i, k);
-                    var distance = euclidianDistance(lastCoordinate, tempCoord);
-                    if (distance > maxDistance) {
-                        maxDistance = distance;
-                        maxCoordinate = tempCoord;
-                    }
-                }
-            }
-
-            return new GameMove(neighbourVertex, maxCoordinate);
+        var heuristicsResult = Heuristics.minimizeHeuristic(g, usedCoordinates, vertexCoordinates, gameMoves, placedVertices, width, height);
+        if (heuristicsResult.isPresent()) {
+            return heuristicsResult.get();
         }
 
         // In this case we can compute the minimal crossings
@@ -296,12 +151,9 @@ public class RTreePlayer implements Player {
      * @return A game move of the final decision.
      */
     private GameMove maximizeMove2(Graph g, int[][] usedCoordinates, HashMap<Vertex, Coordinate> vertexCoordinates, List<GameMove> gameMoves, HashSet<Vertex> placedVertices, int width, int height) {
-        if (placedVertices.size() == 0) {
-            // Maximize is the first move of the game.
-            // Therefore, we place the vertex in the middle of the canvas
-            var firstVertex = g.getVertices().iterator().next();
-            var middleCoordinate = new Coordinate(width / 2, height / 2);
-            return new GameMove(firstVertex, middleCoordinate);
+        var heuristicResult = Heuristics.maximizeHeuristic(g, usedCoordinates, vertexCoordinates, gameMoves, placedVertices, width, height);
+        if (heuristicResult.isPresent()) {
+            return heuristicResult.get();
         }
 
         // In this case we can compute the max. crossings
@@ -354,15 +206,7 @@ public class RTreePlayer implements Player {
         return new GameMove(unplacedVertex, maxCoord);
     }
 
-    /**
-     * Calculate the euclidian distance between two coordinates as integer value.
-     * @param p A coordinate object.
-     * @param q A coordinate object.
-     * @return The distance as integer.
-     */
-    private int euclidianDistance(Coordinate p, Coordinate q) {
-        return (int)Math.sqrt(Math.pow(q.getX() - p.getX(), 2) + Math.pow(q.getY() - p.getY(), 2));
-    }
+
 
     @Override
     public String getName()
