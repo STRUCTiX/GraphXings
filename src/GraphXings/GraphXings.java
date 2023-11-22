95787cc7f5896742dfff80469004016938c8b804
structix <janek@schoff.it>
2023-11-22 15:42:45 +0100

    Update GraphXings exec

--- src/GraphXings/GraphXings.java
+++ src/GraphXings/GraphXings.java
@@ -77,8 +77,8 @@ public class GraphXings
         //}
         //System.out.println("Total games: " + gamesNum + ", Player 1 wins: " + player1 + ", Player 2 wins: " + player2);
 
-        var rcf = new RandomCycleFactory(19111991);
-        var match = new NewMatch(new NewRandomPlayer("RandomPlayer"), new RTreePlayer("RTreePlayer"), rcf, 10);
+        var rcf = new RandomCycleFactory(21122012, true);
+        var match = new NewMatch(new RTreePlayer("RtreePlayer1"), new RTreePlayer("RTreePlayer"), rcf, 20);
         var result = match.play();
         System.out.println(result.announceResult());
     }
