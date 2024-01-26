2dbcb38cbeaf75944e3373e932b15881c70fc7a6
Tobias Stumpp <tobias.stumpp@student.uni-tuebingen.de>
2024-01-26 21:32:23 +0100

    Ignore logs directory without files

--- src/GraphXings/Gruppe4/Gui/Bash/iterate_graph_rendering.sh
+++ src/GraphXings/Gruppe4/Gui/Bash/iterate_graph_rendering.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-GRAPH_BIN="../Python/graph.py"
+PYTHON_BIN="python3"
+GRAPH_SCRIPT="../Python/graph.py"
 LOG_DIR="../../../../../logs"
 NO_OF_THREADS=6
 SECONDS_OF_SLEEP=60
@@ -11,14 +12,16 @@ while true; do
     if [ "$(jobs | wc -l)" -lt ${NO_OF_THREADS} ]; then
         # Find files matching the pattern 1*M*.txt
         files=(${LOG_DIR}/1*M*.txt)
-        # Check if any files exist
-        if [ ${#files[@]} -gt 0 ]; then
+        # Check if any files exist (that aren't just the literal pattern)
+        if [ ${#files[@]} -gt 0 ] && [ "${files[0]}" != "${LOG_DIR}/1*M*.txt" ] ; then
             f="$(basename "${files[0]}")"
             d="${LOG_DIR}/${f//.txt}"
-            mkdir -p "${d}"
+            strategy="$(echo "${f}" | grep -oE 'M[A-Z_]{3,}')"
+            mkdir -p "${d}" "${LOG_DIR}/${strategy}"
             mv "${LOG_DIR}/${f}" "${d}/"
+            ln -s "../${f//.txt}" "${LOG_DIR}/${strategy}/"
             # Launch a new command in the background
-            python "${GRAPH_BIN}" "${d}/${f}" &
+            "${PYTHON_BIN}" "${GRAPH_SCRIPT}" "${d}/${f}" &
         else
             # No matching files found, break out of the loop
             break
