ab901088e3729092fed0ec0c22e4571a62a50c41
Tobias Stumpp <tobias.stumpp@student.uni-tuebingen.de>
2024-01-26 22:34:53 +0100

    Expand oneliner to several lines

--- src/GraphXings/Gruppe4/Gui/Bash/iterate_png_to_mp4.sh
+++ src/GraphXings/Gruppe4/Gui/Bash/iterate_png_to_mp4.sh
@@ -3,4 +3,9 @@
 LOG_DIR="../../../../../logs"
 
 # https://stackoverflow.com/questions/24961127/how-to-create-a-video-from-images-with-ffmpeg
-for d in "${LOG_DIR}/"1*M*/ ; do pushd "${d}" ; f="$(basename ${d})"; ffmpeg -framerate 30 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p "${f}.mp4" ; popd ; done
+for d in "${LOG_DIR}/"1*M*/ ; do
+    pushd "${d}"
+    f="$(basename ${d})"
+    ffmpeg -framerate 30 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p "${f}.mp4"
+    popd
+done
